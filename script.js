52d5125325c4d2bcc17f8df14aa51b53b09ed541
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-01-02 22:14:20 +0700

    Merevisi ulang style.css mengenai tampilan opening

--- script.js
+++ script.js
@@ -1,28 +1,37 @@
-// make element of opening
-function makeOpening(el) {
-    return `<div class="opening-container">
-                <div class="title>
-                    <div class="title-img">
-                        <img src="assets/lamp.png" alt="background">
-                    </div>
-                    <h1>${el["Title"]}</h1>
-                </div>
-                <button>Start Game</button>
-            </div>`;
-}
-
-// request data element opening
-function requestOpening() {
+// request data 
+function requestData() {
     return fetch('main.json')
             .then(response => response.json())
             .then(response => response);
 }
 
+// make element of opening
+function makeOpening(el) {
+    return `<div class="opening-container">
+                <div class="title-img">
+                    <img src="${el.ImageTitle}" alt="background">
+                </div>
+                <h1>${el.Title}</h1>
+                <button>Start Game</button>
+            </div>`;
+}
+
+
+
+// 
 
 // load game
-window.addEventListener('load', () => {
+window.addEventListener('load', async () => {
 
     // add opening section
+    const body = document.querySelector('body');
+
+    let openingData = await requestData();
+    let elemen = makeOpening(openingData);
+    
+    body.innerHTML = elemen;
+
+    console.log(elemen);
     
 
 })
