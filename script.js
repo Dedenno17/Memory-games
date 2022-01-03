030f871c8bd396f2678c505114a6a86a765fe30b
Dedenno17 <ahmaddedensetiawan98@gmail.com>
2022-01-03 14:29:02 +0700

    Membuat element main-game, kemudian menstylekannya

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ function makeOpening(el) {
             </div>`;
 }
 
+
 //show element opening and play the music
 async function showOpening() {
     
@@ -32,6 +33,7 @@ async function showOpening() {
     sound.play();
     sound.volume = 0.3;
 
+    // add click sound
     const startGameBtn = document.querySelector('button');
     startGameBtn.addEventListener('click', ()=> {
         const soundClick = document.querySelector('.click-sound');
@@ -40,14 +42,101 @@ async function showOpening() {
 }
 
 
+// make element of main game
+function makeMain() {
+    return `<div class="main-container">
+                    <div class="score">
+                    <h1>Score</h1>
+                    <span>20</span>
+                </div>
+                <div class="game-container">
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                    <div class="card">
+                        <img src="assets/bee.png" class="front" alt="front-img">
+                        <img src="assets/back.jpg" class="back" alt="back-img">
+                    </div>
+                </div>
+                <div class="time">
+                    <h1>Time Limit</h1>
+                    <span class="limit">30</span>
+                    <button>
+                        <i class="fas fa-pause"></i>
+                        <span>Pause</span>
+                    </button>
+                </div>
+            </div>`;
+}
 
-// 
+
 
 // load game
 window.addEventListener('load', async () => {
 
     //add element to body
-    showOpening();
+    // showOpening();
+    const body = document.querySelector('body');
+    let main = makeMain();
 
-    // 
+    body.innerHTML = main;
+    
+    console.log(makeMain());
 })
